5ab55e0b96c7ee77f11a3816bd627ec784d91b07
Rasmus Billgren <rasmusbillgren89@gmail.com>
2026-05-13 17:24:33 +0200

    wrappade routerprovider i AuthProvider

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,24 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import './css/index.css'
-import App from './App.jsx'
-import routes from './routes.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import "./css/index.css";
+import App from "./App.jsx";
+import routes from "./routes.jsx";
+import { AuthProvider } from "./context/AuthContext";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
     children: routes.map(({ path, element }) => ({
-      path: path === '/' ? undefined : path,
-      index: path === '/',
+      path: path === "/" ? undefined : path,
+      index: path === "/",
       element,
     })),
   },
-])
+]);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>,
-)
+);
