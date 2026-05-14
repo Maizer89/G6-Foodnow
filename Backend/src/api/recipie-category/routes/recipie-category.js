2dc41b4b007ef161321a8e54072608b4ab81afc8
Rasmus Billgren <rasmusbillgren89@gmail.com>
2026-05-14 12:30:20 +0200

    Skapade kategorier för recept för lättare filtrering

---
+++ Backend/src/api/recipie-category/routes/recipie-category.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * recipie-category router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::recipie-category.recipie-category');
---
+++ Backend/src/api/recipie-category/services/recipie-category.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * recipie-category service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::recipie-category.recipie-category');
