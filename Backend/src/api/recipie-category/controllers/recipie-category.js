2d4fe65d7f0239c370ba83f13889c58132d35177
Rasmus Billgren <rasmusbillgren89@gmail.com>
2026-05-15 12:21:37 +0200

    Fixade felstavning i lista och ändrade alla relaterade rader i backend manuellt

--- Backend/src/api/recipie-category/controllers/recipie-category.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * recipie-category controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::recipie-category.recipie-category');
--- Backend/src/api/recipie-category/services/recipie-category.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * recipie-category service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::recipie-category.recipie-category');
