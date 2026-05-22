919857014c9bd05bf2ad6053615a503a861f3f03
Amir <amir@example.com>
2026-05-22 13:57:53 +0200

    Add image attribute to content type definition

--- Backend/types/generated/contentTypes.d.ts
+++ Backend/types/generated/contentTypes.d.ts
@@ -526,6 +526,7 @@ export interface ApiRecipeCategoryRecipeCategory
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
+    image: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
       'oneToMany',
