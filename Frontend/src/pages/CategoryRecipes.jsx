8ece97dbdb9b54eb640d23f8b570fc0ee35765b3
Rasmus Billgren <rasmusbillgren89@gmail.com>
2026-05-24 15:59:51 +0200

    La till kort komponenten i CategoryRecipes och tog bort gammal kod

--- Frontend/src/pages/CategoryRecipes.jsx
+++ Frontend/src/pages/CategoryRecipes.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
+import RecipeCard from "../components/RecipeCard";
 
 const API_URL = "http://localhost:1337";
 const PAGE_SIZE = 20;
@@ -74,30 +75,9 @@ function CategoryRecipes() {
       <div className="recipe-count">{recipes.length} recept hittades</div>
 
       <div className="recipe-grid">
-        {recipes.map((recipe) => {
-          const imageUrl = recipe.image?.url
-            ? `${API_URL}${recipe.image.url}`
-            : "/placeholder-recipe.jpg";
-
-          return (
-            <Link
-              key={recipe.documentId || recipe.id}
-              to={`/recipes/${recipe.documentId || recipe.id}`}
-              className="recipe-card"
-            >
-              <img src={imageUrl} alt={recipe.title} />
-
-              <div className="recipe-content">
-                <h2 className="recipe-title">{recipe.title}</h2>
-
-                <div className="recipe-meta">
-                  <span>{recipe.cooking_time_minutes} min</span>
-                  <span>{recipe.ingredients?.length || 0} ingredienser</span>
-                </div>
-              </div>
-            </Link>
-          );
-        })}
+        {recipes.map((recipe) => (
+          <RecipeCard key={recipe.documentId || recipe.id} recipe={recipe} />
+        ))}
       </div>
 
       {page < pageCount && (
